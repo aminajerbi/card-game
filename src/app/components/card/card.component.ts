6c120998c89b915032ba342ee45ad52ad93fabc0
Amina JERBI <amina.jerbi.2014@gmail.com>
2022-02-03 00:15:52 +0100

    gestion pli et gagnant

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,12 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Card } from 'src/app/card';
 import { Player } from 'src/app/player';
 
+import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  subjectNotifier: Subject<null> = new Subject<null>();
+  pliWinner: Player = new Player('');
   cardsuits = ['trèfle', 'pique', ' coeur', 'carreau'];
   cardranks = [
     '2',
@@ -34,12 +38,53 @@ export class CardComponent implements OnInit {
   constructor() {}
 
   public distributeCards() {
+    let tmpCards: Card[] = [...this.cards];
     for (let player of this.players) {
+      player.resetCards();
       for (let i = 0; i < 13; i++) {
-        let cardIndex = Math.floor(Math.random() * this.cards.length);
-        let card = this.cards[cardIndex];
+        let cardIndex = Math.floor(Math.random() * tmpCards.length);
+        let card = tmpCards[cardIndex];
         player.addCard(card);
-        this.cards.splice(cardIndex, 1);
+        tmpCards.splice(cardIndex, 1);
+      }
+    }
+  }
+
+  private comparePlayers(player1: Player, player2: Player) {
+    let winner: Player;
+    let res = Card.compare(player1.getPlayedCard(), player2.getPlayedCard());
+    if (res >= 0) {
+      winner = player1;
+    } else {
+      winner = player2;
+    }
+    return winner;
+  }
+  // la card la plus forte
+  public pli() {
+    // play cards (just update cardIndex random)
+    for (let player of this.players) {
+      player.playCard();
+    }
+
+    // identify the winner of the pli
+    // first semi-final
+    let winner12: Player = this.comparePlayers(
+      this.players[0],
+      this.players[1]
+    );
+    // second semi-final
+    let winner34: Player = this.comparePlayers(
+      this.players[2],
+      this.players[3]
+    );
+    // final pli winner
+    this.pliWinner = this.comparePlayers(winner12, winner34);
+
+    // transfer cards from losers to winner
+    for (let loser of this.players) {
+      if (!(loser === this.pliWinner)) {
+        loser.transferPlayedCard(this.pliWinner);
       }
     }
   }
@@ -51,12 +96,15 @@ export class CardComponent implements OnInit {
         this.cards.push(card);
       }
     }
-
-   // this.distributeCards();
   }
 
   onDistribute() {
     this.distributeCards();
-     
+    this.subjectNotifier.next(null);
+  }
+
+  onPli() {
+    this.pli();
+    this.subjectNotifier.next(null);
   }
 }
--- src/app/player.ts
+++ src/app/player.ts
@@ -1,21 +1,46 @@
-import { Card } from "./card";
+import { Card } from './card';
 
 export class Player {
-    cards: Card[]=[];
-    name : string = "";
+  resetCards() {
+    this.cards = [];
+  }
+  cards: Card[] = [];
+  name: string = '';
+  cardIndex: number = -1;
 
-    constructor(name:string){
-        this.name= name;
-    }
-    public addCard(card:Card){
-        this.cards.push(card);
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
+  public addCard(card: Card) {
+    this.cards.push(card);
+  }
 
-    public toString() : string{
-        let description = this.name + ":";
-        for (let card of this.cards){
-            description += card.toString()+ " , ";
-        } 
-        return description;
+  public toString(): string {
+    let description = this.name + ':';
+    for (let card of this.cards) {
+      description += card.toString() + ' , ';
     }
+    return description;
+  }
+
+  //question 4
+  public playCard() {
+    if (this.cards.length > 0) {
+      this.cardIndex = Math.floor(Math.random() * this.cards.length);
+    }
+  }
+
+  public transferPlayedCard(winner: Player) {
+    let card: Card = this.getPlayedCard();
+    winner.addCard(card);
+    this.removePlayedCard();
+  }
+
+  public getPlayedCard(): Card {
+    return this.cards[this.cardIndex];
+  }
+
+  public removePlayedCard() {
+    this.cards.splice(this.cardIndex, 1);
+  }
 }
